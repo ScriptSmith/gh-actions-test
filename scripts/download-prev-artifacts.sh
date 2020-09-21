e935e86fd0783230f38d4de038f34f2e70188cfe
ScriptSmith <ScriptSmith@users.noreply.github.com>
2020-09-21 12:10:11 +1000

    Check for prev images

--- scripts/download-prev-artifacts.sh
+++ scripts/download-prev-artifacts.sh
@@ -6,6 +6,11 @@ IMAGES_URL=$(curl -L "$ARTIFACTS_URL" | jq -r '[.artifacts[] | select(.name=="bu
 echo "Artifacts: $ARTIFACTS_URL"
 echo "Images: $IMAGES_URL"
 
+if [[ "$IMAGES_URL" == "null" ]]; then
+    echo "No previous run"
+    exit 0
+fi
+
 curl -L $IMAGES_URL -o prev_run.zip
 unzip prev_run.zip -d prev_run
 
